d18638669746ef49f06dc614b5a760ffce8ac1f0
Ilya Sovtsov <ilyasovty@gmail.com>
2023-04-04 18:20:28 +0400

    More cli arguments, check whether the file is accessible

--- src/main.cc
+++ src/main.cc
@@ -1,3 +1,4 @@
+#include <cstring>
 #include <iostream>
 
 #include "Driver/Driver.hh"
@@ -5,8 +6,18 @@
 
 i32 main(i32 argc, char **argv) {
     if (argc > 1) {
-        Language::Driver::Driver driver{argv[1], argc, argv};
-        driver.Run();
+        if (std::strcmp(argv[1], "--help") == 0) {
+            std::cout << "Usage:\nlanguage filename (arg)\n\nAvailable "
+                         "arguments:\n--tokens - print tokens from file\n--ast - print "
+                         "Abstract Sybtax Tree\n--help - see this help\n\nWarning: do "
+                         "NOT mix arguments\n";
+        } else if (access(argv[1], F_OK) == -1) {
+            std::cerr << "File with name " + std::string{argv[1]} +
+                             " is not accessible\n";
+        } else {
+            Language::Driver::Driver driver{argv[1], argc, argv};
+            driver.Run();
+        }
     } else {
         std::cerr << "No input file was provided\n";
     }
